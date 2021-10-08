13c4c68bc78a2b7bff7f94bff8f482a1c0e9a72a
gomgam <gam@prontomarketing.com>
2021-10-08 11:16:00 +0700

    Add Python tests

--- python/test_main.py
+++ python/test_main.py
@@ -6,3 +6,6 @@ from main import add
 class AddTest(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1, 2), 3)
+
+    def test_add_2(self):
+        self.assertEqual(add(3, 2), 5)
